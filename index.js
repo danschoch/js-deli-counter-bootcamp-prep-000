f80bdfee027474b5fbea1541b4091268030f8da9
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 21:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function nowServing (katzDeliLine) {
 }
 
 function currentLine (katzDeliLine) {
+  var i = 0;
+  
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
