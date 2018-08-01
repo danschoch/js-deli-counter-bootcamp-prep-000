cba1dcca07c74ff0e0819ab47bceb4cb2a59f3f6
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 20:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ function takeANumber (katzDeliLine, name) {
 }
 
 function nowServing (katzDeliLine) {
-  while (katzDeliLine.length > 0) {
+  if (katzDeliLine.length = 0) {
+    return katzDeliLine.shift();
+    
+    
+    while (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`;
   }
-  if (katzDeliLine.length > 0) {
-    katzDeliLine.shift()
-    return katzDeliLine;
+    
   } else {
     return "There is nobody waiting to be served!";
   }
