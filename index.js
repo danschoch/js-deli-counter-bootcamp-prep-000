916ae060f214c81b692a9876aa323c068430f078
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 21:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function takeANumber (katzDeliLine, name) {
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length = 0) {
     return "There is nobody waiting to be served!";
-  } else {
-    return `Currenty serving ${katzDeliLine.shift()}.`;
   }
+  return `Currenty serving ${katzDeliLine.shift()}.`;
 }
