f89fe8b8fc978a995296d42ffd7c417747a05ef5
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 21:26:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function nowServing (katzDeliLine) {
 function currentLine (katzDeliLine) {
   var i = 0;
   var order = i+1;
+  var lineString = 
   
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
