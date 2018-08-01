442cabe47c6f0e6aaf32b5c3b3dee13a844468bb
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 20:40:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing (katzDeliLine) {
+  while (katzDeliLine.length > 0) {
+    return katzDeliLine [0];
+  }
+  if (katzDeliLine.length > 0) {
+    katzDeliLine.pop();
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
